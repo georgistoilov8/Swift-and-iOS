b6a2cef842b2e86e5aa2e42c319cecf7e33b1ac9
Georgi Stoilov <gogo.stoilov@abv.bg>
2018-11-15 00:11:08 +0200

    Simple navigation

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Georgi Stoilov on 14.11.18.
+//  Copyright © 2018 Georgi Stoilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
